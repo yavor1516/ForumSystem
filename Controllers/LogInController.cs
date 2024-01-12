6913b6162f98252118e7be236c506c63b2a317ce
Yavor Manev <yavor.manev.a54@learn.telerikacademy.com>
2024-01-12 15:03:06 +0200

    Register page and LogIn page

---
+++ Controllers/LogInController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForumSystem.Controllers
+{
+    public class LogInController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
