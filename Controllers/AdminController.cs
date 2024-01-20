f8af38bc1faa23eef9258e4eb50d5285e28f4dab
NikolaySemkov <nsemkov00@gmail.com>
2024-01-20 16:02:40 +0200

    admin panel added /admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using ForumSystem.Helpers;
+using ForumSystem.Models;
+using ForumSystem.Repositories;
+using ForumSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IAdminPanelService _adminPanelService;
+        private readonly IUserRepository _userRepository;
+
+        public AdminController(IAdminPanelService adminPanelService, IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _adminPanelService = adminPanelService;
+        }
+
+       
+        public IActionResult Index()
+        {
+            var viewModel = new AdminViewModel
+            {
+                Users = _userRepository.GetAllUsers()
+
+            };
+            return View(viewModel);
+            
+        }
+    }
+}
---
+++ Models/AdminViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ForumSystem.Models
+{
+    public class AdminViewModel
+    {
+        public ICollection<User> ? Users { get; set; }
+    }
+}
